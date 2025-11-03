86fd6f0e3a261fe67481b9e63c7b5c88181eb7e6
AnkithJoseph <ankithjoseph@hotmail.com>
2025-11-03 22:18:45 +0000

    resend update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import Resend from 'resend';
+import { Resend } from 'resend';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
